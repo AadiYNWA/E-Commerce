e88ee8eba520373855559beb34a752d7b302c52a
AadiYNWA <aadeshagte@gmail.com>
2024-09-10 13:41:30 +0530

    Footer Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import Policy from '../components/Policy'
+import News from '../components/News'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero/>
       <LatestCollection/>
       <BestSeller/>
+      <Policy/>
+      <News/>
     </div>
   )
 }
