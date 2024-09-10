3855fa8ed6b8d89f995afe544c2f7da1498cb4ee
AadiYNWA <aadeshagte@gmail.com>
2024-09-10 12:14:26 +0530

    BestSeller Section

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ const LatestCollection = () => {
     return (
         <div className='my-10'>
             <div className='text-center py-8 text-3xl'>
-                <Title text1={'LATEST '} text2={'COLLECTION'} />
+                <Title text1={'LATEST'} text2={'COLLECTION'} />
                 <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad repellendus sed error hic iure quia molestiae vero placeat voluptas voluptatibus blanditiis odio quis corrupti, laboriosam similique corporis facere, eius necessitatibus.
                 </p>
@@ -24,7 +24,7 @@ const LatestCollection = () => {
             <div className='grid grid-cold-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
                     latestProducts.map((item, index)=>(
-                        <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
                     ))
                 }
             </div>
