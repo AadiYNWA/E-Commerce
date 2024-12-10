189ef541357631be20f52124b0854decce3dd008
AadiYNWA <aadeshagte@gmail.com>
2024-12-10 16:40:20 +0400

    User Controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+// Add Product
+const addProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// List Product
+const listProducts = async (req, res) => {
+
+}
+
+// Delete Product
+const removeProduct = async (req, res) => {
+
+}
+
+// Single Product Detail
+const singleProduct = async (req, res) => {
+
+}
+
+export {addProduct, listProducts, removeProduct, singleProduct}
