8798f37a966da65bfdd9b3a9b04174bc0f3442b1
AadiYNWA <aadeshagte@gmail.com>
2024-12-11 13:36:28 +0400

    Admin Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,30 +5,30 @@ import jwt from "jsonwebtoken"
 
 
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: '1d' });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 }
 
 
 // Route for User Login
 const loginUser = async (req, res) => {
     try {
-        const {email, password} = req.body;
-        const user = await userModel.findOne({email});
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
         if (!user) {
-            return res.json({success:false, message:"User Does Not Exist"});
+            return res.json({ success: false, message: "User Does Not Exist" });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (isMatch) {
             const token = createToken(user._id);
-            res.json({success:true, token});
+            res.json({ success: true, token });
         }
-        else{
-            return res.json({success:false, message:"Invalid Credentials"});
+        else {
+            return res.json({ success: false, message: "Invalid Credentials" });
         }
     } catch (error) {
         console.log(error);
-        res.json({success:false, message : error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -54,21 +54,33 @@ const registerUser = async (req, res) => {
 
         const newUser = new userModel({
             name, email,
-            "password" : hashedPassword
+            "password": hashedPassword
         });
         const user = await newUser.save();
         const token = createToken(user._id);
-        res.json({success:true, token})
+        res.json({ success: true, token })
 
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // Route for Admin Login
 const adminLogin = async (req, res) => {
-
+    try {
+        const { email, password } = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({ success: true, token });
+        }
+        else {
+            res.json({ success: false, message: "Invalid Credentials" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 export { loginUser, registerUser, adminLogin }
